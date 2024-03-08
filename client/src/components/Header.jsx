20f56bf3e0122c14075c4a5d38f477831071b638
João Victor Gadelha <joaovictor_gadelha@live.com>
2024-03-07 22:26:32 -0300

    feat: Estruturando Header component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Logo from '../images/logo.png'
+import { FaBars } from 'react-icons/fa'
+import { AiOutlineClose } from 'react-icons/ai'
 
 const Header = () => {
 	return (
 		<nav>
 			<div className='container nav__container'>
 				<Link to='/' className='nav__logo'>
-					<img src={Logo} alt='}Navbar Logo' />
+					<img src={Logo} alt='Navbar Logo' />
 				</Link>
+				<ul className='nav__menu'>
+					<li>
+						<Link to='/profile'>Ernest Achiever</Link>
+						<Link to='/create'>Create Post</Link>
+						<Link to='/authors'>Authors</Link>
+						<Link to='/logout'>Logout</Link>
+					</li>
+				</ul>
+				<button className='nav__toggle-btn'>
+					<AiOutlineClose />
+				</button>
 			</div>
 		</nav>
 	)
