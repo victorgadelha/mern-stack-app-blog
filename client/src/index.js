3fa61a8d514bd6dd1f1931d84c21dbfe5447ec34
João Victor Gadelha <joaovictor_gadelha@live.com>
2024-03-07 21:49:06 -0300

    feat: Criando rotas com o react-router-dom

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,48 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+import Layout from './components/Layout'
+import ErrorPage from './pages/ErrorPage'
+import Home from './pages/Home'
+import PostDetail from './pages/PostDetail'
+import Register from './pages/Register'
+import Login from './pages/Login'
+import UserProfile from './pages/UserProfile'
+import Author from './pages/Authors'
+import CreatePost from './pages/CreatePost'
+import EditPost from './pages/EditPost'
+import CategoryPosts from './pages/CategoryPosts'
+import AuthorPosts from './pages/AuthorPosts'
+import DashBoard from './pages/DashBoard'
+import Logout from './pages/Logout'
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Layout />,
+		errorElement: <ErrorPage />,
+		children: [
+			{ index: true, element: <Home /> },
+			{ path: 'posts/:id', element: <PostDetail /> },
+			{ path: 'register', element: <Register /> },
+			{ path: 'login', element: <Login /> },
+			{ path: 'profile/:id', element: <UserProfile /> },
+			{ path: 'authors', element: <Author /> },
+			{ path: 'create', element: <CreatePost /> },
+			{ path: 'posts/categories/:category', element: <CategoryPosts /> },
+			{ path: 'posts/users/:id', element: <AuthorPosts /> },
+			{ path: 'myposts/:id', element: <DashBoard /> },
+			{ path: 'posts/:id/edit', element: <EditPost /> },
+			{ path: 'logout', element: <Logout /> },
+		],
+	},
+])
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
-    <h1>Hello World</h1>
-  </React.StrictMode>
-);
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
+)
